9bf15ac2b6faec87dda86fe5d7b6f76450617541
Yan Palmer <86450167+YanPalmer@users.noreply.github.com>
2023-10-04 00:07:01 -0300

    Express configurado

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+// import express from "express";
+
+// const app = express();
+
+const express = require("express");
+const app = express();
+
+// Configura o express para utilizar JSON
+app.use(express.json());
